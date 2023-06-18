786c2155144d379d786446f31be397cee2fd0526
Tom Xu <thomas.w.xu@gmail.com>
2023-06-18 15:17:03 -0400

    Add fractal tiling

--- sun_halo.glsl
+++ sun_halo.glsl
@@ -8,8 +8,8 @@ vec3 OrangeBluePalette(in float centerDist)
 {
     vec3 a = vec3(0.500, 0.500, 0.500);
     vec3 b = vec3(0.500, 0.500, 0.500);
-    vec3 c = vec3(1.00, 0.800, 0.500);
-    vec3 d = vec3(0.000, 0.200, 0.500);
+    vec3 c = vec3(1.0, 1.0, 1.0); // vec3(1.00, 0.800, 0.500);
+    vec3 d = vec3(0.25, 0.4, 0.55); // vec3(0.000, 0.200, 0.500);
     return a + b * cos(6.28318 * (c * centerDist + d));
 }
 
@@ -56,6 +56,17 @@ float RepeatedRings(in float centerDist, in float frequency, in float thickness_
 }
 
 /**
+ * Convert from normalized, centered coordinates to tiled, centered coordinates.
+ *
+ * @param normCoord Normalized, centered coordinate.
+ * @param tileFactor Factor to use for fractional tiling. Use a higher value for more tiles.
+ */
+vec2 Normalized2TiledCoord(in vec2 normCoord, in float tileFactor)
+{
+    return fract(normCoord * tileFactor) - 0.5;
+}
+
+/**
  * This function is run for every pixel (standard function signature from GLSL).
  */
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
@@ -65,16 +76,25 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     normCoord = (normCoord - 0.5) * 2.0; // Move origin to center
     normCoord.x *= iResolution.x / iResolution.y; // Fix aspect ratio
 
-    // Precomputed useful quantities
-    float centerDist = length(normCoord);
-    vec3 colorBalance = OrangeBluePalette(centerDist); //vec3(3.0, 2.0, 1.0);
+    // Drawing loop
+    vec3 finalColor = vec3(0.0);
+    vec2 tiledCoord = normCoord;
+    for (float i = 0.0; i < 3.0; i++) {
+        // Tiled normalized coordinates
+        tiledCoord = Normalized2TiledCoord(tiledCoord, 1.7);
 
-    // Draw ring
-    float ringDist = Ring(centerDist, 0.5, 0.05, 0.0);
+        // Precomputed useful quantities
+        float centerDist = length(normCoord);
+        float tiledCenterDist = length(tiledCoord);
 
-    float repeatedRingDist = RepeatedRings(centerDist, 4.0, 0.0, 0.5, true, true);
+        // Draw rings
+        float repeatedRingDist = RepeatedRings(tiledCenterDist, 7.0, 0.0, 0.5, true, false);
 
-    fragColor = vec4(colorBalance * repeatedRingDist, 1.0);
-//    fragColor = vec4(ringDist, ringDist, 0.0, 1.0);
+        // Colors
+        vec3 centerColorWave = OrangeBluePalette(centerDist - iTime * 0.5);
+        finalColor += centerColorWave * repeatedRingDist;    
+    }
 
+
+    fragColor = vec4(finalColor, 1.0);
 }
